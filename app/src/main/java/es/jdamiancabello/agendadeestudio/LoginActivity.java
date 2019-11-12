6903f85b35abe65b6e392d143128983e6768bc8a
Damián Cabello <jdamiancabello@gmail.com>
2019-11-12 14:43:13 +0100

    Primera version del login.

---
+++ app/src/main/java/es/jdamiancabello/agendadeestudio/LoginActivity.java
@@ -0,0 +1,29 @@
+package es.jdamiancabello.agendadeestudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView signUp_text = findViewById(R.id.signUp_text);
+        signUp_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                Animatoo.animateSwipeLeft(LoginActivity.this);
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/es/jdamiancabello/agendadeestudio/RegisterActivity.java
@@ -0,0 +1,29 @@
+package es.jdamiancabello.agendadeestudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        TextView signIn_text = findViewById(R.id.signIn_text);
+        signIn_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                Animatoo.animateSwipeRight(RegisterActivity.this);
+                finish();
+            }
+        });
+    }
+}
