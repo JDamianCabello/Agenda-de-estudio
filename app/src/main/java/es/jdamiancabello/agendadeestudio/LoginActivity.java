20cae339157e757ee6068d8790cb6411ebd8ebe7
Damián Cabello <jdamiancabello@gmail.com>
2019-11-19 11:54:54 +0100

    Ahora sí.

--- app/src/main/java/es/jdamiancabello/agendadeestudio/LoginActivity.java
+++ app/src/main/java/es/jdamiancabello/agendadeestudio/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,6 +15,7 @@ public class LoginActivity extends AppCompatActivity {
     private ImageView ivBack;
     private TextView tvRegister;
     private TextView tvHelp;
+    private Button register_btLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +47,17 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this, AboutMe.class).addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT));
                 Animatoo.animateSlideRight(LoginActivity.this);
+            }
+        });
+
+
+        register_btLogin = findViewById(R.id.register_btLogin);
+        register_btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+                Animatoo.animateSlideRight(LoginActivity.this);
+                finish();
             }
         });
     }
