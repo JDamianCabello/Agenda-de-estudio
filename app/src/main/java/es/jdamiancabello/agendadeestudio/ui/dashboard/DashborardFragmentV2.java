a11aadfd6c59fcafa4975821cc6bd11d4e447e76
Damián Cabello <jdamiancabello@gmail.com>
2020-01-16 23:25:38 +0100

    empezado dashboard v2

---
+++ app/src/main/java/es/jdamiancabello/agendadeestudio/ui/dashboard/DashborardFragmentV2.java
@@ -0,0 +1,59 @@
+package es.jdamiancabello.agendadeestudio.ui.dashboard;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import es.jdamiancabello.agendadeestudio.R;
+
+
+public class DashborardFragmentV2 extends Fragment {
+    public static final String TAG = "DashborardFragmentV2";
+    private OnFragmentInteractionListener mListener;
+
+    public static DashborardFragmentV2 newInstance() {
+        DashborardFragmentV2 fragment = new DashborardFragmentV2();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashborard_fragment_v2, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
