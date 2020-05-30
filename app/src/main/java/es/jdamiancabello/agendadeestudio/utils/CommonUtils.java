448438fcd69c14c6ddd4290654cdcd99fa161f1b
Damián Cabello <jdamiancabello@gmail.com>
2020-05-30 20:24:59 +0200

    Nuevo fragment de asignaturas

--- app/src/main/java/es/jdamiancabello/agendadeestudio/utils/CommonUtils.java
+++ app/src/main/java/es/jdamiancabello/agendadeestudio/utils/CommonUtils.java
@@ -2,6 +2,10 @@ package es.jdamiancabello.agendadeestudio.utils;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -29,7 +33,70 @@ public final class CommonUtils {
 
     public static boolean patterUser(String user){
         final String PATTERN = ".{6,20}$";
-
         return user.matches(PATTERN);
+    }
+
+    /**
+     * Devuelve un long desde una fecha en formato dd-MM-yyyy
+     *
+     * @param dateString Es el string que contiene la fecha
+     * @return
+     */
+    public static long dateStringToLong(String dateString){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar calendar = Calendar.getInstance();
+        try {
+            calendar.setTime(simpleDateFormat.parse(dateString));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return calendar.getTimeInMillis();
+    }
+
+    /**
+     * Devuelve un long desde una fecha especificando el formato
+     *
+     * @param dateString Es el string que contiene la fecha
+     * @param dateFormat Es el formato de fecha que quiere que devolvamos
+     * @return
+     */
+    public static long dateStringToLong(String dateString, String dateFormat){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
+        Calendar calendar = Calendar.getInstance();
+        try {
+            calendar.setTime(simpleDateFormat.parse(dateString));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return calendar.getTimeInMillis();
+    }
+
+    /**
+     * Devuelve un string con el formato fecha dd-MM-yyyy desde un long
+     *
+     * @param dateLong Es el long que contiene la fecha
+     * @return
+     */
+    public static String dateLongToString(Long dateLong){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(dateLong);
+
+        return simpleDateFormat.format(calendar.getTime());
+    }
+
+    /**
+     * Devuelve un long desde una fecha especificando el formato
+     *
+     * @param dateLong Es el long que contiene la fecha
+     * @param dateFormat Es el formato de fecha que quiere que devolvamos
+     * @return Un string en el formato de dateformat
+     */
+    public static String dateLongToString(Long dateLong, String dateFormat){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(dateLong);
+
+        return simpleDateFormat.format(calendar.getTime());
     }
 }
